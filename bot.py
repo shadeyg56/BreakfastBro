b69045bee4a36700ab543bcc6fd0a7aee98bd448
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-07 15:00:16 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,19 @@ if 'TOKEN' in os.environ:
     
 bot = commands.Bot(prefix='tbd')
 
+@bot.event
+async def on_ready():
+    print('------------------------------------')
+    print('THE BOT IS ONLINE')
+    print('------------------------------------')
+    print("Name: {}".format(bot.user.name))
+    print('Author: shadeyg56')
+    print("ID: {}".format(bot.user.id))
+    print('DV: {}'.format(discord.__version__))
+    
+@bot.command(pass_context=True)
+async def test(ctx):
+    await bot.say('All systems operational')
+
 bot.run(TOKEN)
 
