7d725a9f585db4ff7ee2f92cffbe67ed4eb41c66
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-07 16:48:02 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def order(ctx, *, food: str):
     embed.set_footer(text='From: {} | {}'.format(ctx.message.server, ctx.message.server.id))
     await bot.send_message(kitchen, embed=embed)
     with open('ids.json') as f:
-        json.loads(f.read())
+        data = json.loads(f.read())
         data[ctx.message.author.id]['id'] = id2
         data = json.dumps(data, indent=4, sort_keys=True)
     with open('ids.json', 'w') as f:
