bba05a2daa03a6019bbed3f723cb0ef6a9d7f893
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-08 16:55:46 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ async def order(ctx, *, food: str):
     num = random.randint(0, 100)
     kitchen = bot.get_channel('366325015488233493')
     id2 = id[num]
-    bot.customer = ctx.message.user.id
+    bot.customer = ctx.message.author.id
     await bot.say('Got it. Headed to the kitchen now. Your order ID is {}'.format(id2))
     embed = discord.Embed(title='New Order, ID: {}'.format(id2), description=food, color=0xed)
     embed.set_author(name='{} | {}'.format(ctx.message.author, ctx.message.author.id), icon_url=ctx.message.author.avatar_url)
