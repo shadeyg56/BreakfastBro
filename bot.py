6d25b2e7fdac8a8c1076bc8de1cdfbaa256677ba
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-09 09:01:36 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def order(ctx, *, food: str):
     embed.set_author(name='{} | {}'.format(ctx.message.author, ctx.message.author.id), icon_url=ctx.message.author.avatar_url)
     embed.set_footer(text='From: {} | {}'.format(ctx.message.server, ctx.message.server.id))
     await bot.send_message(kitchen, embed=embed)
-    data = json.loads(open('ids.json')
+    data = json.loads(open('ids.json').read())
     data[user.id]["unclaimed"] = id2
     data = json.dumps(data, indent=4, sort_keys=True)
     with open('ids.json', 'w') as f:
