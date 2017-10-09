713cef618ecb3f974a067fb70603ad12594ea0b4
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-09 11:24:15 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ async def claim(ctx, orderid: str):
     if data[user.id]["status"] == "unclaimed":
         if data[user.id]["orderid"] == '{}'.format(orderid):
             data[user.id]["status"] = "claimed"
-            await bot.say('{0.mention}, You claimed order {1}'.format(ctx.message.auuthor, orderid))
+            await bot.say('{0.mention}, You claimed order {1}'.format(ctx.message.author, orderid))
         else:
              await bot.say("That order doesn\'t exist")
     else:
